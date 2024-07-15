02d77a0e48188bba97a6be9212f78c40ff456841
jayxalvarado <jayxalvarado@gmail.com>
2024-07-15 16:51:40 -0700

    feat: Add currency selection functionality to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import arrow_icon from "../../assets/arrow_icon.png";
+import { CoinContext } from "../../context/CoinContext";
 
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+
+  const currencyChangeHandler = (e) => {
+    switch (e.target.value) {
+      case "usd":
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      case "euro":
+        setCurrency({ name: "eur", symbol: "€" });
+        break;
+      case "jpy":
+        setCurrency({ name: "jpy", symbol: "¥" });
+        break;
+      case "gbp":
+        setCurrency({ name: "gbp", symbol: "£" });
+        break;
+      case "aud":
+        setCurrency({ name: "aud", symbol: "A$" });
+        break;
+      case "cad":
+        setCurrency({ name: "cad", symbol: "C$" });
+        break;
+
+      default: {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+    }
+  };
+
   return (
     <div className="navbar">
       <img src={logo} alt="logo" className="logo" />
@@ -17,7 +48,8 @@ const Navbar = () => {
 
       {/* right-side */}
       <div className="nav-right">
-        <select>
+        {/* currency change handler  */}
+        <select onChange={currencyChangeHandler}>
           <option value="usd">USD</option>
           <option value="euro">EUR</option>
           <option value="jpy">JPY</option>
