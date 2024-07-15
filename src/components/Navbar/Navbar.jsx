05a5b073a753276d1bce44800483821677bbfb18
jayxalvarado <jayxalvarado@gmail.com>
2024-07-15 09:51:53 -0700

    chore: Update Navbar styling and add currency options

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import arrow_icon from "../../assets/arrow_icon.png";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img src={logo} alt="logo" />
+      <img src={logo} alt="logo" className="logo" />
       <ul>
         <li>Home</li>
         <li>Features</li>
@@ -19,7 +19,10 @@ const Navbar = () => {
         <select>
           <option value="usd">USD</option>
           <option value="euro">EUR</option>
-          <option value="pesos">PESOS</option>
+          <option value="jpy">JPY</option>
+          <option value="gbp">GBP</option>
+          <option value="aud">AUD</option>
+          <option value="cad">CAD</option>
         </select>
         <button>
           Sign Up <img src={arrow_icon} alt="arrow icon" />
