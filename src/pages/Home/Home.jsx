a272b467cbb3ece0b8d934e7ffb5904055198319
jayxalvarado <jayxalvarado@gmail.com>
2024-07-15 13:20:41 -0700

    chore: Update page title, navbar, and hero section styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,20 +4,35 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home">
-      <div lassName="hero">
+      <div className="hero">
         <h1>
           Largest <br /> Crypto Center!
         </h1>
         <p>
-          Welcome to the world's largest learning center for Cryptocurrency!
-          Come see why we've been voted rank #11 for the past 40 years (way
-          before Crypto even existed including the person responsible making
-          this webpage)
+          Welcome to the world's largest knowledge center for Cryptocurrency!
+          Come see why we've been voted the world's rank #9th for the past 69
+          years!
+          <br />
+          <br />
+          Sign up now to learn more about the world of Cryptocurrency and
+          explore <em>some</em> of our features!
         </p>
+        {/* search */}
         <form>
           <input type="text" placeholder="Search crypto..." />
           <button type="submit">Search</button>
         </form>
+      </div>
+      {/* hero coin table */}
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p>24H Change</p>
+          <p>24H Change %</p>
+          <p>Total MC</p>
+        </div>
       </div>
     </div>
   );
